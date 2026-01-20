f277432fd391f043f4429196b7c1087debca4294
solohubivan <solohubivan@gmail.com>
2026-01-20 15:59:17 +0200

    Initial Commit

---
+++ Lunio Days/App/LunioDaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lunio_DaysApp.swift
+//  Lunio Days
+//
+//  Created by Ivan Solohub on 20.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct LunioDaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Lunio Days/AppDescribeScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  AppDescribeScreenView.swift
+//  Lunio Days
+//
+//  Created by Ivan Solohub on 20.01.2026.
+//
+
+import SwiftUI
+
+struct AppDescribeScreenView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+#Preview {
+    AppDescribeScreenView()
+}
